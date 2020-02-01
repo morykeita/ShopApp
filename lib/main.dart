32f6576228da37a0b26dd03341188f58b8514d78
morykeita <morykeita@protonmail.com>
2020-01-31 23:52:05 -0500

    adding provider, refactoring application

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,27 @@
 import 'package:flutter/material.dart';
-
-import './screens/products_overview_screen.dart';
-
+import './providers/products_provider.dart';
 import './screens/product_detail_screen.dart';
+import './screens/products_overview_screen.dart';
+import 'package:provider/provider.dart';
 
-void main()=> runApp(MyApp());
+void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'MyShop',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-        accentColor: Colors.deepOrangeAccent,
-        
+    return ChangeNotifierProvider(
+      builder: (ctx) => ProductsProvider(),
+      child: MaterialApp(
+        title: 'MyShop',
+        theme: ThemeData(
+          primarySwatch: Colors.purple,
+          accentColor: Colors.deepOrangeAccent,
+        ),
+        home: ProductOverviewScreen(),
+        routes: {
+          ProductDetailScreen.routeName: (context) => ProductDetailScreen(),
+        },
       ),
-      home: ProductOverviewScreen() ,
-      routes: {
-        ProductDetailScreen.routeName : (context) => ProductDetailScreen(),
-      },
     );
   }
-  
 }
--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import '../providers/products_provider.dart';
+import 'package:provider/provider.dart';
 
 class ProductDetailScreen extends StatelessWidget {
-
 //  final String title;
 //  final double price;
 //
 //  ProductDetailScreen({this.title,this.price});
-static const String routeName = 'productDetailRoute';
+  static const String routeName = 'productDetailRoute';
   @override
   Widget build(BuildContext context) {
-   final productId =  ModalRoute.of(context).settings.arguments as String; //
+    final productId = ModalRoute.of(context).settings.arguments as String; //
+    final loadedProduct = Provider.of<ProductsProvider>(context, listen: false)
+        .findById(productId); // don't listen to updates
     return Scaffold(
       appBar: AppBar(
-        title: Text('title') ,
+        title: Text(loadedProduct.title),
       ),
     );
   }
