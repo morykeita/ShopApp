9beb330a7a86f0ff55acfefdf5f68f06d5569e5a
morykeita <morykeita@protonmail.com>
2020-02-02 02:06:26 -0500

    added cart functionality and product overview screen

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,26 +1,30 @@
 import 'package:flutter/material.dart';
+import '../providers/product.dart';
+import 'package:provider/provider.dart';
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  ProductItem({@required this.id, @required this.title, this.imageUrl});
+//  final String id;
+//  final String title;
+//  final String imageUrl;
+//
+//  ProductItem({@required this.id, @required this.title, this.imageUrl});
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
           child: GestureDetector(
             onTap: () {
-              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,arguments: id);
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
 //              Navigator.of(context).push(
 //                  MaterialPageRoute(builder: (ctx) => ProductDetailScreen(title: title,)));
             },
             child: Image.network(
-              imageUrl,
+              product.imageUrl,
               fit: BoxFit.cover,
             ),
           ),
@@ -32,7 +36,7 @@ class ProductItem extends StatelessWidget {
               color: Theme.of(context).accentColor,
             ),
             title: Text(
-              title,
+              product.title,
               textAlign: TextAlign.center,
             ),
             trailing: IconButton(
